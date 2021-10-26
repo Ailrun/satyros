d6fd22b7da49e8d0cb8c68aa0a7be0b48b4b1c15
Junyoung/Clare Jang <jjc9310@gmail.com>
2021-10-26 17:15:43 -0400

    Fix CNF top module

--- src/Satyros/CNF.hs
+++ src/Satyros/CNF.hs
@@ -1,13 +1,13 @@
 module Satyros.CNF
-  ( module Satyros.CNF.Variable
-  , module Satyros.CNF.Positivity
-  , module Satyros.CNF.Literal
-  , module Satyros.CNF.Clause
+  ( module Satyros.CNF.Clause
   , module Satyros.CNF.Formula
+  , module Satyros.CNF.Literal
+  , module Satyros.CNF.Positivity
+  , module Satyros.CNF.Variable
   ) where
 
-import Satyros.CNF.Variable
-import Satyros.CNF.Positivity
-import Satyros.CNF.Literal
-import Satyros.CNF.Clause
-import Satyros.CNF.Formula
+import           Satyros.CNF.Clause
+import           Satyros.CNF.Formula
+import           Satyros.CNF.Literal
+import           Satyros.CNF.Positivity
+import           Satyros.CNF.Variable
