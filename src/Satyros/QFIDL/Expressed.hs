7d633c11cba2c4c123252defe6bf872660963028
Junyoung/Clare Jang <jjc9310@gmail.com>
2021-10-26 21:17:45 -0400

    Fix code style

--- src/Satyros/QFIDL/Expressed.hs
+++ src/Satyros/QFIDL/Expressed.hs
@@ -1,6 +1,6 @@
 module Satyros.QFIDL.Expressed where
 
-import           Satyros.QFIDL.Variable    (Variable)
+import           Satyros.QFIDL.Variable (Variable)
 
 data Expressed
   = LessThanEqualTo Variable Variable Int
