3a8f46eeabecd2f00bbc3ee74c5388d342cf86b5
Junyoung/Clare Jang <jjc9310@gmail.com>
2021-11-11 03:46:12 -0500

    Update Haskell code

--- haskell/src/Satyros/Handler/Naive.hs
+++ haskell/src/Satyros/Handler/Naive.hs
@@ -1,6 +1,6 @@
 module Satyros.Handler.Naive where
 
-import           Control.Lens               (Lens', _1, _2, _3, use, uses, view,
+import           Control.Lens               (Lens', _1, _2, _3, _4, use, uses, view,
                                              (%~), (.=))
 import           Control.Monad.State.Strict (runState, state)
 import           Control.Monad.Trans.Free   (hoistFreeT, transFreeT)
@@ -12,7 +12,7 @@ import           Satyros.DPLL               (DPLL, DPLLF)
 import qualified Satyros.DPLL               as DPLL
 import qualified Satyros.QFIDL              as QFIDL
 
-type InternalStorage = (QFIDL.ConversionTable, BellmanFord.IDLGraph, BellmanFord.Storage)
+type InternalStorage = (QFIDL.ConversionTable, BellmanFord.IDLGraph, BellmanFord.Storage, (BellmanFord.IDLGraphVertex , BellmanFord.IDLGraphVertex))
 type Storage = DPLL.Storage InternalStorage
 
 naiveHandler :: DPLLF BellmanFordF
@@ -27,26 +27,39 @@ naiveHandler DPLL.DecisionComplete = do
   m <- use (DPLL.theory . _1)
   (g, w) <- uses DPLL.assignment $
     BellmanFord.initializeStorage . QFIDL.fromAssignment m . fmap (_2 %~ view _1) . Map.toAscList . DPLL.getAssignment
+  DPLL.theory . _2 .= g
   DPLL.theory . _3 .= w
   liftBellmanFord _3 $ BellmanFord.propagation g
   pure False
 naiveHandler DPLL.BacktraceExhaustion = pure False
 naiveHandler (DPLL.BacktraceComplete c l) = DPLL.backtraceCompleteHandler c l >> DPLL.decision >> pure False
-naiveHandler (DPLL.InsideDPLL (BellmanFord.PropagationCheck _ r)) = r
+naiveHandler (DPLL.InsideDPLL (BellmanFord.PropagationCheck e r)) = do
+  DPLL.theory . _4 .= e
+  r
 naiveHandler (DPLL.InsideDPLL (BellmanFord.PropagationFindShorter _ _ r)) = r
-naiveHandler (DPLL.InsideDPLL (BellmanFord.PropagationNth _ r)) = r
+naiveHandler (DPLL.InsideDPLL (BellmanFord.PropagationNth _ r)) = do
+  DPLL.theory . _4 .= (BellmanFord.rootIDLGraphVertex, BellmanFord.rootIDLGraphVertex)
+  r
 naiveHandler (DPLL.InsideDPLL BellmanFord.PropagationEnd) = do
-  m <- use (DPLL.theory . _1)
-  (g, _) <- uses DPLL.assignment $
-    BellmanFord.initializeStorage . QFIDL.fromAssignment m . fmap (_2 %~ view _1) . Map.toAscList . DPLL.getAssignment
+  DPLL.theory . _4 .= (BellmanFord.rootIDLGraphVertex, BellmanFord.rootIDLGraphVertex)
+  g <- use (DPLL.theory . _2)
   liftBellmanFord _3 $ BellmanFord.negativeCycle g
   pure False
-naiveHandler (DPLL.InsideDPLL (BellmanFord.NegativeCycleCheck _ r)) = r
+naiveHandler (DPLL.InsideDPLL (BellmanFord.NegativeCycleCheck e r)) = do
+  DPLL.theory . _4 .= e
+  r
 naiveHandler (DPLL.InsideDPLL (BellmanFord.NegativeCycleFind c)) = do
+  DPLL.theory . _2 .= Map.empty
+  DPLL.theory . _3 .= Map.empty
+  DPLL.theory . _4 .= (BellmanFord.rootIDLGraphVertex, BellmanFord.rootIDLGraphVertex)
   m <- use (DPLL.theory . _1 . _2)
   DPLL.bcpConflictRelSATHandler $ CNF.Clause (fmap (m Map.!) c)
   pure False
-naiveHandler (DPLL.InsideDPLL BellmanFord.NegativeCyclePass) = pure True
+naiveHandler (DPLL.InsideDPLL BellmanFord.NegativeCyclePass) = do
+  DPLL.theory . _2 .= Map.empty
+  DPLL.theory . _3 .= Map.empty
+  DPLL.theory . _4 .= (BellmanFord.rootIDLGraphVertex, BellmanFord.rootIDLGraphVertex)
+  pure True
 
 liftBellmanFord :: Lens' s BellmanFord.Storage -> BellmanFord a -> DPLL s BellmanFordF a
 liftBellmanFord l =
--- haskell/src/Satyros/UI.hs
+++ haskell/src/Satyros/UI.hs
@@ -8,44 +8,52 @@ module Satyros.UI
 
 import           Prelude                     hiding ((!!))
 
-import           Control.Lens                (Each (each), _1, at, from, head1,
-                                              partsOf, to, (&), (?~), (^.))
+import           Control.Lens                (_1, _Just, _Wrapped', at, each,
+                                              filtered, from, head1, ix,
+                                              partsOf, singular, to, (^.),
+                                              (^..), (^?))
 import           Control.Monad               (void)
 import           Control.Monad.Reader        (liftIO)
 import           Control.Monad.Trans.Free    (FreeF (Free, Pure))
+import           Data.Bifunctor              (bimap, first)
 import           Data.Either.Extra           (fromRight')
 import           Data.Functor.Compose        (Compose (Compose, getCompose))
 import           Data.IORef.Extra            (IORef, modifyIORef', newIORef,
-                                              readIORef)
+                                              readIORef, writeIORef)
 import           Data.List.NonEmpty          (NonEmpty ((:|)))
 import qualified Data.List.NonEmpty          as NE
 import qualified Data.Map                    as Map
-import           Data.Maybe                  (fromJust)
+import           Data.Maybe                  (fromJust, maybeToList)
+import qualified Data.Set                    as Set
+import qualified Data.Vector                 as Vector
 import           GHC.Generics                (Generic)
 import           Language.Javascript.JSaddle (FromJSVal (fromJSVal), Function,
                                               JSM, JSString, JSVal, ToJSVal,
-                                              call, global, js, js1,
-                                              jsUndefined, toJSVal, (!!), (!))
+                                              call, global, js, js0, js1,
+                                              toJSVal, (!!), (!))
 import qualified Satyros.BellmanFord         as BellmanFord
 import           Satyros.BellmanFord.Effect  (BellmanFordF)
 import qualified Satyros.CNF                 as CNF
-import           Satyros.DPLL                (DPLL)
+import qualified Satyros.CNF                 as DPLL
+import           Satyros.DPLL                (DPLL, DPLLF)
 import qualified Satyros.DPLL                as DPLL
-import           Satyros.Handler.Naive       (naiveHandler)
+import           Satyros.Handler.Naive       (InternalStorage, Storage,
+                                              naiveHandler)
 import qualified Satyros.QFIDL               as QFIDL
 import           Satyros.UI.Util             (function0, function1, function2)
-import           System.Random.Stateful      (getStdGen)
-
-type InternalStorage = (QFIDL.ConversionTable, BellmanFord.IDLGraph, BellmanFord.Storage)
-type Storage = DPLL.Storage InternalStorage
+import           System.Random.Stateful      (mkStdGen)
 
 data SatyrosAPI
   = SatyrosAPI
-    { myfun            :: Function
-    , expressedFormula :: CNF.FormulaLike QFIDL.Expressed
-    , conversionTable  :: SatyrosConversionTable
-    , assignment       :: SatyrosAssignmentAPI
-    , step             :: Function
+    { myfun               :: Function
+    , expressedFormula    :: CNF.FormulaLike QFIDL.Expressed
+    , getFormula          :: Function
+    , conversionTable     :: SatyrosConversionTable
+    , assignment          :: SatyrosAssignmentAPI
+    , getImplicationGraph :: Function
+    , getBellmanFordGraph :: Function
+    , step                :: Function
+    , undo                :: Function
     }
   deriving stock (Generic)
   deriving anyclass (ToJSVal)
@@ -60,22 +68,64 @@ data SatyrosConversionTable
 
 data SatyrosAssignmentAPI
   = SatyrosAssignmentAPI
-    { getValue :: Function
-    , setValue :: Function
+    { getValue          :: Function
+    , getValueOfClause  :: Function
+    , getValueOfFormula :: Function
     }
   deriving stock (Generic)
   deriving anyclass (ToJSVal)
 
+data SatyrosImplicationVertex
+  = SatyrosImplicationVertex
+    { variable   :: CNF.Variable
+    , value      :: Bool
+    , isDecision :: Bool
+    , level      :: Int
+    }
+  deriving stock (Generic)
+  deriving anyclass (ToJSVal)
+
+data SatyrosImplicationEdge
+  = SatyrosImplicationEdge
+    { startVertex :: CNF.Variable
+    , endVertex   :: CNF.Variable
+    , levelDiff   :: Int
+    , clauseIndex :: Int
+    }
+  deriving stock (Generic)
+  deriving anyclass (ToJSVal)
+
+data SatyrosBellmanFordVertex
+  = SatyrosBellmanFordVertex
+    { variable :: BellmanFord.IDLGraphVertex
+    , distance :: Int
+    }
+  deriving stock (Generic)
+  deriving anyclass (ToJSVal)
+
+data SatyrosBellmanFordEdge
+  = SatyrosBellmanFordEdge
+    { startVertex :: BellmanFord.IDLGraphVertex
+    , endVertex   :: BellmanFord.IDLGraphVertex
+    , weight      :: Int
+    , lastActive  :: Bool
+    }
+  deriving stock (Generic)
+  deriving anyclass (ToJSVal)
+
+type SatyrosAPIHistory = NonEmpty (Storage, Maybe (DPLLF BellmanFordF (DPLL InternalStorage BellmanFordF Bool)), DPLL InternalStorage BellmanFordF Bool)
+
 makeSatyrosAPI :: JSM (JSVal -> JSM JSVal)
 makeSatyrosAPI = pure $ \jf -> do
   f <- fromJust <$> fromJSVal jf
-  stdGen <- getStdGen
   let
+    stdGen = mkStdGen 2
     (cnf, t) = QFIDL.toCNF f
-    s = fromRight' $ DPLL.initializeStorage cnf stdGen (t, Map.empty, Map.empty)
-  tsRef <- liftIO $ newIORef ((s, DPLL.bcp >> pure False) :| [])
+    s = fromRight' $ DPLL.initializeStorage cnf stdGen (t, Map.empty, Map.empty, (BellmanFord.rootIDLGraphVertex, BellmanFord.rootIDLGraphVertex))
+  tsRef <- liftIO $ newIORef ((s, Nothing, DPLL.bcp >> pure False) :| [])
   myfun <- function0 (\_ _ -> global ^. js ("console" :: JSString) . js1 ("log" :: JSString) f >> pure ())
   step <- function1 $ makeStep tsRef
+  undo <- function1 $ makeUndo tsRef
   let
     expressedFormula =
       cnf
@@ -92,35 +142,148 @@ makeSatyrosAPI = pure $ \jf -> do
         )
       . from CNF.clauseLikesOfFormulaLike
     conversionTable = SatyrosConversionTable (Map.toList $ fst t) (Map.toList $ snd t)
-  assignment <- makeAssignment tsRef
+  getFormula <- makeGetFormula tsRef
+  assignment <- makeAssignmentAPI tsRef
+  getImplicationGraph <- makeGetImplicationGraph tsRef
+  getBellmanFordGraph <- makeGetBellmanFordGraph tsRef
   toJSVal SatyrosAPI{..}
 
-makeStep :: IORef (NonEmpty (Storage, DPLL InternalStorage BellmanFordF Bool)) -> JSVal -> JSVal -> JSVal -> JSM ()
-makeStep tsRef _ this cb = do
-  (s, next) :| _ <- liftIO $ readIORef tsRef
+makeStep :: IORef SatyrosAPIHistory -> JSVal -> JSVal -> JSVal -> JSM ()
+makeStep tsRef _ _ cb = do
+  (s, _, next) :| _ <- liftIO $ readIORef tsRef
   let
     (feff, s') = DPLL.stepDPLL next s
   case feff of
-    Pure b   -> void $ call cb this [b]
+    Pure b   -> do
+      liftIO $ modifyIORef' tsRef ((s', Nothing, pure b) NE.<|)
+      void $ cb ^. js1 ("Finish" :: JSString) b
     Free eff -> do
-      liftIO $ modifyIORef' tsRef ((s', naiveHandler eff) NE.<|)
-      void $ call cb this [jsUndefined]
+      liftIO $ modifyIORef' tsRef ((s', Just eff, naiveHandler eff) NE.<|)
+      invokeEffectCallback cb eff
 
-makeAssignment :: IORef (NonEmpty (Storage, DPLL InternalStorage BellmanFordF Bool)) -> JSM SatyrosAssignmentAPI
-makeAssignment tsRef = do
+makeUndo :: IORef SatyrosAPIHistory -> JSVal -> JSVal -> JSVal -> JSM ()
+makeUndo tsRef _ _ cb = do
+  _ :| ts <- liftIO $ readIORef tsRef
+  case ts of
+    [] -> void $ cb ^. js1 ("Start" :: JSString) True
+    t'@(_, meff, _) : ts' -> do
+      liftIO $ writeIORef tsRef (t' :| ts')
+      case meff of
+        Just eff -> invokeEffectCallback cb eff
+        Nothing -> void $ cb ^. js1 ("Start" :: JSString) False
+
+invokeEffectCallback :: JSVal -> DPLLF BellmanFordF a -> JSM ()
+invokeEffectCallback cb DPLL.BCPUnitClause{} = void $ cb ^. js0 ("BCPUnitClause" :: JSString)
+invokeEffectCallback cb DPLL.BCPComplete = void $ cb ^. js0 ("BCPComplete" :: JSString)
+invokeEffectCallback cb DPLL.BCPConflict{} = void $ cb ^. js0 ("BCPConflict" :: JSString)
+invokeEffectCallback cb DPLL.BCPConflictDrivenClause{} = void $ cb ^. js0 ("BCPConflictDrivenClause" :: JSString)
+invokeEffectCallback cb DPLL.DecisionResult{} = void $ cb ^. js0 ("DecisionResult" :: JSString)
+invokeEffectCallback cb DPLL.DecisionComplete = void $ cb ^. js0 ("DecisionComplete" :: JSString)
+invokeEffectCallback cb DPLL.BacktraceExhaustion = void $ cb ^. js0 ("BacktraceExhaustion" :: JSString)
+invokeEffectCallback cb DPLL.BacktraceComplete{} = void $ cb ^. js0 ("BacktraceComplete" :: JSString)
+invokeEffectCallback cb (DPLL.InsideDPLL BellmanFord.PropagationCheck{}) = void $ cb ^. js0 ("PropagationCheck" :: JSString)
+invokeEffectCallback cb (DPLL.InsideDPLL BellmanFord.PropagationFindShorter{}) = void $ cb ^. js0 ("PropagationFindShorter" :: JSString)
+invokeEffectCallback cb (DPLL.InsideDPLL BellmanFord.PropagationNth{}) = void $ cb ^. js0 ("PropagationNth" :: JSString)
+invokeEffectCallback cb (DPLL.InsideDPLL BellmanFord.PropagationEnd) = void $ cb ^. js0 ("PropagationEnd" :: JSString)
+invokeEffectCallback cb (DPLL.InsideDPLL BellmanFord.NegativeCycleCheck{}) = void $ cb ^. js0 ("NegativeCycleCheck" :: JSString)
+invokeEffectCallback cb (DPLL.InsideDPLL BellmanFord.NegativeCycleFind{}) = void $ cb ^. js0 ("NegativeCycleFind" :: JSString)
+invokeEffectCallback cb (DPLL.InsideDPLL BellmanFord.NegativeCyclePass) = void $ cb ^. js0 ("NegativeCyclePass" :: JSString)
+
+makeAssignmentAPI :: IORef SatyrosAPIHistory -> JSM SatyrosAssignmentAPI
+makeAssignmentAPI tsRef = do
   getValue <- function2 makeGetValue
-  setValue <- function2 makeSetValue
+  getValueOfClause <- function2 makeGetValueOfClause
+  getValueOfFormula <- function1 makeGetValueOfFormula
   pure SatyrosAssignmentAPI{..}
   where
-    makeGetValue _ this jx c = do
+    makeGetValue _ this jx cb = do
       x <- fromJust <$> fromJSVal jx
       ts <- liftIO $ readIORef tsRef
-      void $ call c this [ts ^. head1 . _1 . DPLL.assignment . at x]
-    makeSetValue _ _ jx jv = do
-      x <- fromJust <$> fromJSVal jx
-      v <- fromJust <$> fromJSVal jv
-      liftIO $ modifyIORef' tsRef $ \ts ->
-        ts & head1 . _1 . DPLL.assignment . at x ?~ v
+      void $ call cb this [ts ^. head1 . _1 . DPLL.assignment . at x]
+
+    makeGetValueOfClause _ this ji cb = do
+      i <- fromJust <$> fromJSVal ji
+      ts <- liftIO $ readIORef tsRef
+      let
+        s = ts ^. head1 . _1
+        asgn = s ^. DPLL.assignment
+        cv = s ^. DPLL.clauses . partsOf (ix i . DPLL.literalsOfClause . each) . to (valueOfLiterals asgn)
+      void $ call cb this [cv]
+
+    makeGetValueOfFormula _ this cb = do
+      ts <- liftIO $ readIORef tsRef
+      let
+        s = ts ^. head1 . _1
+        asgn = s ^. DPLL.assignment
+        c = s ^. DPLL.clauses . to Vector.toList . partsOf (each . DPLL.literalsOfClause . to (fmap not . valueOfLiterals asgn)) . to (fmap not . firstTrue)
+      void $ call cb this [c]
+
+    valueOfLiterals asgn ls = firstTrue $ fmap (\l -> asgn ^? DPLL.valueOfLiteral l) ls
+
+    firstTrue [] = Just False
+    firstTrue (lv : lvs)
+      | Just True <- lv = Just True
+      | Just False <- lv = rest
+      | Just False <- rest = Nothing
+      | otherwise = rest
+      where
+        rest = firstTrue lvs
+
+makeGetFormula :: IORef SatyrosAPIHistory -> JSM Function
+makeGetFormula tsRef = do
+  function1 makeGet
+  where
+    makeGet _ this cb = do
+      ts <- liftIO $ readIORef tsRef
+      void $ call cb this [ts ^. head1 . _1 . DPLL.clauses . to Vector.toList . from CNF.clausesOfFormula]
+
+makeGetImplicationGraph :: IORef SatyrosAPIHistory -> JSM Function
+makeGetImplicationGraph tsRef = do
+  function1 makeGetVerticesEdges
+  where
+    makeGetVerticesEdges _ this cb = do
+      ts <- liftIO $ readIORef tsRef
+      let
+        s = ts ^. head1 . _1
+        leveledVs =
+          concatMap (\(level, vs) -> fmap (\((variable, value), isDecision) -> SatyrosImplicationVertex{..}) vs)
+          . zip [(0 :: Int)..]
+          . reverse
+          . fmap (fmap (first (\x -> (x, s ^. DPLL.assignment . singular (DPLL.valueOfVariable x)))) . uncurry (<>) . bimap (fmap (,True) . maybeToList) (fmap (,False) . Set.toList))
+          $ s ^. DPLL.variableLevels
+        varToLevel = Map.fromList $ (\SatyrosImplicationVertex{..} -> (variable, level)) <$> leveledVs
+        impls =
+          concatMap (\(startVertex, (_, c)) ->
+                        c ^.. _Just . CNF.literalsOfClause . each . to CNF.literalToVariable . filtered (/= startVertex) . to (makeEdge varToLevel (s ^. DPLL.clauses . to (fromJust . Vector.elemIndex (fromJust c))) startVertex))
+          $ s ^. DPLL.assignment . _Wrapped' . to Map.toList
+      void $ call cb this [toJSVal leveledVs, toJSVal impls]
+
+    makeEdge varToLevel clauseIndex startVertex endVertex =
+      let
+        levelDiff = varToLevel Map.! endVertex - varToLevel Map.! startVertex
+      in
+        SatyrosImplicationEdge{..}
+
+makeGetBellmanFordGraph :: IORef SatyrosAPIHistory -> JSM Function
+makeGetBellmanFordGraph tsRef = do
+  function1 makeGetVerticesEdges
+  where
+    makeGetVerticesEdges _ this cb = do
+      ts <- liftIO $ readIORef tsRef
+      let
+        (_, g, m, lastActiveE) = ts ^. head1 . _1 . DPLL.theory
+        vs =
+          (\(variable, (_, distance)) -> SatyrosBellmanFordVertex{..}) <$> Map.toList m
+        es =
+          filter (\SatyrosBellmanFordEdge{..} -> startVertex /= endVertex)
+          . fmap (\((startVertex, endVertex), weight) ->
+                     let
+                       lastActive = lastActiveE == (startVertex, endVertex)
+                     in
+                       SatyrosBellmanFordEdge{..}
+                 )
+          $ Map.toList g
+      void $ call cb this [toJSVal vs, toJSVal es]
 
 instance FromJSVal CNF.Variable where
   fromJSVal = fmap (fmap CNF.Variable) . fromJSVal
