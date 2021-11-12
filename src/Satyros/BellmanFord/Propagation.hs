3586658a3c1a25c089c089b72c92599ac28ab93e
Junyoung/Clare Jang <jjc9310@gmail.com>
2021-11-11 22:05:08 -0500

    Optimize propagation loop num

--- src/Satyros/BellmanFord/Propagation.hs
+++ src/Satyros/BellmanFord/Propagation.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE ViewPatterns #-}
 module Satyros.BellmanFord.Propagation where
 
-import           Control.Lens                (_1, at, each, uses, (.=), (^..))
+import           Control.Lens                (_1, _2, at, each, uses, (.=), (^..))
 import           Control.Monad               (forM_, when)
 import qualified Data.Map                    as Map
 import           Data.Maybe                  (fromJust)
@@ -14,7 +14,7 @@ import           Satyros.BellmanFord.Storage (IDLGraph)
 
 propagation :: IDLGraph -> BellmanFord ()
 propagation (Map.toList -> graph) = do
-  forM_ [1..Set.size (Set.fromList (graph ^.. each . _1))] $ \n -> do
+  forM_ [1..Set.size (Set.fromList (graph ^.. each . _1 . _2)) - 2] $ \n -> do
     propagationNth n
     forM_ graph $ \((f, t), w) -> do
       propagationCheck (f, t)
